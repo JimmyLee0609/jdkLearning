f2e755446f5714eb384564f61489096c6439e03f
JimmyLee0609 <35453330+JimmyLee0609@users.noreply.github.com>
2018-02-23 18:56:11 +0800

    ReentrantLock

--- jdkLearning/concurrent/lock/ReentrantLockTest.java
+++ jdkLearning/concurrent/lock/ReentrantLockTest.java
@@ -1,32 +1,137 @@
 package lock;
 
+import java.util.LinkedList;
+import java.util.UUID;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ReentrantLockTest {
-	static ReentrantLock lock;
-	Condition c=lock.newCondition();
-	
+
 	public static void main(String[] args) {
-		//排他锁，默认false， 就像线程抢夺那样唤醒全部线程，谁拿到就是谁的
-//		true的情况是，根据线程获取锁的时间顺序依次让它们获取到锁，里面有一个queue来处理
-		lock= new ReentrantLock(true);
-		Runnable task = new Runnable() {
+		/*
+		 * 重进入锁,就是支持重进入的锁, 表示该锁能够支持一个线程对资源的重复加锁, lock里面可以加condition 该锁还支持获取锁的是否FIFO
+		 * 其实concurrent包中的queue就是很好的例子, 下面使用一个简单的队列的例子来看效果
+		 * debug模式下,只要将put的线程停止,队列没有数据,take的线程就会阻塞
+		 * condition相当于将线程放在一个线程组中,可以将指定的组线程唤醒,
+		 * 
+		 * Lock 仅仅锁主某个操作的瞬间,被锁住的瞬间,其他线程无法访问
+		 * 
+		 * ,然后使用condition去操作线程的睡眠,和唤醒,
+		 * 在Condition的状态中,线程是释放了Lock的锁对象,其他线程可以获取到锁,
+		 * 
+		 * 以这样的方式来达到同步的效果
+		 */
+
+		SimpleQueue queue = new SimpleQueue();
+
+		Runnable takeTask = new Runnable() {
 			@Override
 			public void run() {
-				add(5,3);
+				while (true) {
+					String take;
+					try {
+						take = queue.take();
+//						System.out.println(Thread.currentThread().getName()+take);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
 		};
-		Thread t1 = new Thread(task);
-		Thread t2 = new Thread(task);
+		Runnable putTask = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while (true) {
+						queue.put(UUID.randomUUID().toString());
+//						System.out.println(Thread.currentThread().getName()+"put");
+					}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+		// 获取数据的线程
+		Thread t1 = new Thread(takeTask);
+		Thread t2 = new Thread(takeTask);
+		Thread t9 = new Thread(takeTask);
 		t1.start();
 		t2.start();
-	}
-	
-	public static  void add(int a,int b) {
-		lock.lock();
-		System.out.println(a+b);
-		lock.unlock();
+		t9.start();
+		// 添加数据的线程
+		Thread t3 = new Thread(putTask);
+		Thread t4 = new Thread(putTask);
+		t3.start();
+		t4.start();
+
+		System.out.println();
 	}
 
+}
+
+class SimpleQueue {
+	ReentrantLock lock = new ReentrantLock(false);;
+	Condition notFull = lock.newCondition();
+	Condition notEmpty = lock.newCondition();
+	LinkedList<String> list = new LinkedList<String>();
+
+	public SimpleQueue() {
+	}
+
+	public boolean add(String detail) {
+		lock.lock();
+		try {
+			if (list.size() > 10) {
+				return false;
+			}
+			list.addLast(detail);
+			notEmpty.signalAll();
+			System.out.println(Thread.currentThread().getName()+"notEmpty signall");
+			return true;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public void put(String detail) throws InterruptedException {
+		lock.lock();
+		try {
+			while (!add(detail)) {
+				notFull.await();
+				System.out.println(Thread.currentThread().getName()+"not Full await");
+			}
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public String take() throws InterruptedException {
+		lock.lock();
+		try {
+			String get;
+			while (null == (get = get())) {
+				notEmpty.await();
+				System.out.println(Thread.currentThread().getName()+"not Empty await");
+			}
+			return get;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public String get() {
+		lock.lock();
+		try {
+			String first = list.poll();
+			if (first == null)
+				return null;
+			notFull.signalAll();
+			System.out.println(Thread.currentThread().getName()+"notFull signall");
+			return first;
+		} finally {
+			lock.unlock();
+		}
+	}
 }
